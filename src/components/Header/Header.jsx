ca76dfbff995582e972c39d787b0a0ce5f6f84bf
Mdsakib999 <sakibabrar999@gmail.com>
2023-04-01 00:04:49 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className=''>
+            <div className='header'>
+                <h2>Programming Blog</h2>
+                <img src="https://img.freepik.com/free-photo/close-up-portrait-young-bearded-man-white-shirt-jacket-posing-camera-with-broad-smile-isolated-gray_171337-629.jpg?w=996&t=st=1680284707~exp=1680285307~hmac=2bfc012a8477ff1cd9613abdf95bc173b97b2138101c5bc4a30f476711808996" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
