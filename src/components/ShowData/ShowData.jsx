ac3a97d32fb2165be07f70550f1da8e5353676a4
Mdsakib999 <sakibabrar999@gmail.com>
2023-04-01 23:46:17 +0600

    change commit

--- src/components/ShowData/ShowData.jsx
+++ src/components/ShowData/ShowData.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import './ShowData.css'
+import React from "react";
+import "./ShowData.css";
 
-const ShowData = ({data, readTime, bookmark}) => {
-    const { BlogTitle } = data
-    return (
-        <div className='parent-div'>
-            <div className=' showData'>
-                <div>
-                    <img className='cover-img' src={data.coverImage} alt="" />
-                </div>
+const ShowData = ({ data, readTime, bookmark }) => {
+  const { BlogTitle, id } = data;
+  return (
+    <div className="parent-div">
+      <div className=" showData">
+        <div>
+          <img className="cover-img" src={data.coverImage} alt="" />
+        </div>
 
-                <div className='data-text-1'>
-                    <div className='data-text-2'>
-                        <div className='data-text-3'>
-                            <img className='author-img' src={data.authorImg} alt="" />
-                            <div className='data-text-4'>
-                                <h3>{data.authorName}</h3>
-                                <p>{data.publishDate}</p>
-                            </div>
-                        </div>
-                         
-                         <p>{data.readTime} min read
-                         <span onClick={() => bookmark(BlogTitle)} className='icon'> <i class="fa-regular fa-bookmark"></i> </span>
-                         </p>
-                    </div>
-
-                    <h2 className='title'>{data.BlogTitle}</h2>
-
-                    <p onClick={() => readTime(data.readTime)} className='mark-read'>Mark as read</p>
-
-                </div>
+        <div className="data-text-1">
+          <div className="data-text-2">
+            <div className="data-text-3">
+              <img className="author-img" src={data.authorImg} alt="" />
+              <div className="data-text-4">
+                <h3>{data.authorName}</h3>
+                <p>{data.publishDate}</p>
+              </div>
             </div>
 
-            
+            <p>
+              {data.readTime} min read
+              <span onClick={() => bookmark(BlogTitle, id)} className="icon">
+                {" "}
+                <i class="fa-regular fa-bookmark"></i>{" "}
+              </span>
+            </p>
+          </div>
+
+          <h2 className="title">{data.BlogTitle}</h2>
+
+          <p onClick={() => readTime(data.readTime)} className="mark-read">
+            Mark as read
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShowData;
--- src/components/SideCard/SideCard.jsx
+++ src/components/SideCard/SideCard.jsx
@@ -1,45 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import './SideCard.css'
+import React, { useEffect, useState } from "react";
+import "./SideCard.css";
 
-const SideCard = ({reading, BlogTitle}) => {
-    const [time, setTime] =useState(reading)
-    const [breckTitle, setBreckTitle] = useState([]);
-    const [length, setLength] = useState([]);
-    useEffect(() => {
-        const getReadTime = localStorage.getItem("readTime");
-        setTime(getReadTime);
-    }, [reading]);
+const SideCard = ({ reading, BlogTitle }) => {
+  const [time, setTime] = useState(reading);
+  const [breckTitle, setBreckTitle] = useState([]);
+  const [length, setLength] = useState([]);
+  useEffect(() => {
+    const getReadTime = localStorage.getItem("readTime");
+    setTime(getReadTime);
+  }, [reading]);
 
-    useEffect(() => {
-        let getTitleFromLocalStorage = JSON.parse(localStorage.getItem("BlogTitle"));
-        if (getTitleFromLocalStorage) {
-          setBreckTitle(getTitleFromLocalStorage);
-          setLength(getTitleFromLocalStorage.length);
-        }
-      }, [BlogTitle]);
-
-    return (
-        <div className=''>
-            <div className='spent-text'>
-                <h2>Spent time on read: {time}</h2>
-            </div>
-
-            <div className='blog-title b'>
-                <h2>Bookmarked Blog: {length}</h2>
-                <div>
-                {breckTitle.map((blog) => {
-        return <h2 style={{
-            backgroundColor: "yellowgreen",
-            fontSize: "25px",
-            borderRadius: "10px",
-            padding: "10px",
-          }}>{blog}</h2>;
-      })}
-                    
-                </div>
-            </div>
-        </div>
+  useEffect(() => {
+    let getTitleFromLocalStorage = JSON.parse(
+      localStorage.getItem("BlogTitle")
     );
+    if (getTitleFromLocalStorage) {
+      setBreckTitle(getTitleFromLocalStorage);
+      setLength(getTitleFromLocalStorage.length);
+    }
+  }, [BlogTitle]);
+
+  return (
+    <div className="">
+      <div className="spent-text">
+        <h2>Spent time on read: {time}</h2>
+      </div>
+
+      <div className="blog-title b">
+        <h2>Bookmarked Blog: {length}</h2>
+        <div>
+          {breckTitle.map((blog) => {
+            return (
+              <h2
+                style={{
+                  backgroundColor: "#fff",
+                  fontSize: "25px",
+                  borderRadius: "10px",
+                  padding: "10px",
+                }}
+              >
+                {blog}
+              </h2>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideCard;
